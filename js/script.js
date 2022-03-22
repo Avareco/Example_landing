3a5389dd4c8f4b6674fcc6a2f575ec3a69dcab82
Andrii Iakovliev <yk.120301@gmail.com>
2022-03-22 17:25:07 +0200

    finished all

--- js/script.js
+++ js/script.js
@@ -3,6 +3,25 @@ const videoBlock = document.querySelector('#video');
 const play = document.querySelector('#play-button');
 const mediaQuery = window.matchMedia('(min-width: 991px)')
 const navBar = document.querySelector('#nav-bar');
+
+const burger = document.querySelector(".header-menu__btn");
+
+const menu = document.querySelector(".header-menu__box");
+const menuItem = document.querySelector(".header-menu__box")
+const body = document.querySelector("body")
+burger.addEventListener("click", e => {
+	menu.classList.toggle('active')
+	burger.classList.toggle('active')
+	body.classList.toggle('lock')
+})
+
+menuItem.addEventListener('click',e=>{
+	menu.classList.toggle('active')
+	burger.classList.toggle('active')
+	body.classList.toggle('lock')
+})
+
+
 function handleTabletChange(e) {
 	if (!e.matches) {
 		video.setAttribute('poster', 'img/screen-mobile.jpg')
@@ -13,13 +32,15 @@ function handleTabletChange(e) {
 		navBar.style.position = "fixed"
 		window.onscroll = function () {
 			var currentScrollPos = window.pageYOffset;
-			if (prevScrollpos > currentScrollPos) {
-				navBar.style.top = "0";
-			} else {
-				navBar.style.top = "-100%";
-				navBar.style.backgroundColor = "#333"
+			if (!body.classList.contains("lock")) {
+				if (prevScrollpos > currentScrollPos) {
+					navBar.style.top = "0";
+				} else {
+					navBar.style.top = "-100%";
+					navBar.style.backgroundColor = "#333"
+				}
+				prevScrollpos = currentScrollPos;
 			}
-			prevScrollpos = currentScrollPos;
 		}
 
 
